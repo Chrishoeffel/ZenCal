1ef0f16506d92dbddc35ce967de30c9d220b3c08
Chris Ladd <codebyladd@gmail.com>
2021-11-16 20:42:51 -0700

    config static_url

--- calander_app/settings.py
+++ calander_app/settings.py
@@ -154,7 +154,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #     "http://127.0.0.1:3000",
 # ]
 CORS_ALLOW_ALL_ORIGINS: True
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
